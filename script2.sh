43c7327b687de5f155e777f02db58d673b4afc7d
TndsG <106232227+tndsG@users.noreply.github.com>
2024-02-07 17:07:59 -0800

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+################################################################################
+# Copyright (c) [2023] [Ravindu Deshan]
+#
+# Unauthorized publication is prohibited. Forks and personal use are allowed.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND.
+################################################################################
+
+apt-get update -y; apt-get upgrade -y; wget https://raw.githubusercontent.com/sbatrow/DARKSSH-MANAGER/master/Dark; chmod 777 Dark; ./Dark
 
